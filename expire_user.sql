6a1d23482093624bfdbf8249359cb5895c69b321
Eugene Berman <e_berman@comcast.net>
2024-12-24 09:17:04 -0500

    Add files via upload

---
+++ expire_user.sql
@@ -0,0 +1,8 @@
+set echo on term off;
+
+prompt;
+
+alter user &1 password expire;
+
+exit;
+
---
+++ testuser.sql
@@ -0,0 +1,10 @@
+set echo on term off
+
+prompt;
+
+show user
+
+select global_name from global_name;
+
+exit;
+
