290b6f1738ebe48a1f4613cf0289bf0c662e62cd
Eugene Berman <e_berman@comcast.net>
2024-12-24 09:27:27 -0500

    Delete step4_create_MEMBER_HIST_FACT_TO_RESP_PRSN_mask_view.sql

--- step4_create_MEMBER_HIST_FACT_TO_RESP_PRSN_mask_view.sql
+++
@@ -1,9 +0,0 @@
- create or replace view  prod_dw.MEMBER_HIST_FACT_TO_RESP_PRSN_mask
-as
-(select /* full(MHFTRP) */
-MHFTRP.MEMBER_HISTORY_FACT_KEY, 
-case when nvl(masked_ind, 0) = 1  then -99 else RESPONSIBLE_PERSON_KEY end as RESPONSIBLE_PERSON_KEY,
-SORT_ORDER,
-WEIGHT
-from prod_dw.MEMBER_HIST_FACT_TO_RESP_PRSN  MHFTRP
-left outer join prod_dw.all_member_history_Fact mhf on MHFTRP.MEMBER_HISTORY_FACT_KEY = MHF.MEMBER_HISTORY_FACT_KEY);
