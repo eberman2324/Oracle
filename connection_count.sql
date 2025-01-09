32f148d969b6dd88fffaa3c4d7e6d98210937df8
Eugene Berman <e_berman@comcast.net>
2025-01-09 17:01:08 -0500

    Delete connection_count.sql

--- connection_count.sql
+++
@@ -1,5 +0,0 @@
-select count(*) from v$session where machine = 'ADD\MIDPWKABIS01'
-
-select count(*) as Session_Count,machine,program from v$session where machine = 'ADD\MIDPWKABIS01' and
-program = 'dllhost.exe'
-GROUP BY machine,program
