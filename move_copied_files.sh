96d4969ea92c50c136af2fdaac5b9e669b81dd37
Eugene Berman <e_berman@comcast.net>
2025-01-22 13:10:04 -0500

    Add files via upload

---
+++ move_copied_files.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#exit
+
+cd /oracle/hrpmaskbackup/data/HEPYMASK
+
+##mv bkp_df1176466239_s4293_p1 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466239_s4293_p1
+mv bkp_df1176466242_s4294_p1 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p1
+mv bkp_df1176466242_s4294_p10 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p10
+mv bkp_df1176466242_s4294_p11 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p11
+mv bkp_df1176466242_s4294_p12 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p12
+mv bkp_df1176466242_s4294_p13 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p13
+mv bkp_df1176466242_s4294_p14 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p14
+mv bkp_df1176466242_s4294_p15 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p15
+mv bkp_df1176466242_s4294_p16 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p16
+mv bkp_df1176466242_s4294_p17 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p17
+mv bkp_df1176466242_s4294_p18 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p18
+mv bkp_df1176466242_s4294_p19 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p19
+mv bkp_df1176466242_s4294_p2 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p2
+mv bkp_df1176466242_s4294_p20 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p20
+mv bkp_df1176466242_s4294_p21 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p21
+mv bkp_df1176466242_s4294_p22 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p22
+mv bkp_df1176466242_s4294_p23 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p23
+mv bkp_df1176466242_s4294_p24 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p24
+mv bkp_df1176466242_s4294_p25 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p25
+mv bkp_df1176466242_s4294_p26 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p26
+mv bkp_df1176466242_s4294_p27 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p27
+mv bkp_df1176466242_s4294_p28 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p28
+mv bkp_df1176466242_s4294_p29 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p29
+mv bkp_df1176466242_s4294_p3 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p3
+mv bkp_df1176466242_s4294_p30 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p30
+mv bkp_df1176466242_s4294_p31 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p31
+mv bkp_df1176466242_s4294_p32 /oracle/hrpmaskbackup/data/HEPYMASK_KEEP/bkp_df1176466242_s4294_p32
+
+
+
+
+
+
+
