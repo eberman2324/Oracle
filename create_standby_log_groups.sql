86dd2a2f7d4119a66605c5db3a6282551e6aa1b9
Eugene Berman <e_berman@comcast.net>
2025-01-22 07:45:50 -0500

    Delete create_standby_log_groups.sql

--- create_standby_log_groups.sql
+++
@@ -1,4 +0,0 @@
-
-alter database add standby logfile group 13 ('+REDOA_01') size 52428800;        
-alter database add standby logfile group 12 ('+REDOA_01') size 52428800;        
-alter database add standby logfile group 11 ('+REDOA_01') size 52428800;        
