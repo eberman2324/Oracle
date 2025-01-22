54d837c4149d1ff784f8f2df0bf08cf432185975
Eugene Berman <e_berman@comcast.net>
2025-01-22 07:56:05 -0500

    Delete set_stby_mgmt.sql

--- set_stby_mgmt.sql
+++
@@ -1,5 +0,0 @@
-connect sys/locked99999@TSTDB12_xorangw1d as sysdba;
-set echo on
-alter system set standby_file_management = 'AUTO';
-connect sys/locked99999@TSTDB12_xoragdbw1d as sysdba;
-alter system set standby_file_management = 'AUTO';
