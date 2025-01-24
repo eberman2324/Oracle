d26758e4723eeb231c49522dde8f1cc9eba08e0b
Eugene Berman <e_berman@comcast.net>
2025-01-24 08:28:32 -0500

    Delete check_standby_status.sql

--- check_standby_status.sql
+++
@@ -1,4 +0,0 @@
-alter session set nls_date_format='YYYY-MM-DD HH24:MI:SS';
-select state from V$LOGSTDBY_STATE;
-select APPLIED_TIME, applied_scn, LATEST_TIME, latest_scn from v$logstdby_progress;
-
